5265cabbe2b4f30c690a7b5c996819841148fa70
Eli  Irvine <eliirvine23@gmail.com>
2019-11-17 21:49:17 -0700

    done with UpdateSting

--- week12_utility.py
+++ week12_utility.py
@@ -9,3 +9,9 @@ def LoadFile(file_name):
     with open(file_name, 'r') as f:
         content = f.read().splitlines()
         return content
+
+def UpdateString(str_1, str_2, index):
+    first_half = str_1[0:index]
+    second_half = str_1[index+1:]
+    updated_str = first_half + str_2 + second_half
+    PrintOutput(updated_str)
